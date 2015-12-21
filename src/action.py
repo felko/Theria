4465c0d96b663c1c854960a1d7a6283317094c3c
Skolopedrion <alpha.zeta546@gmail.com>
2015-12-21 21:17:02 +0100

    Improved Action class

--- src/action.py
+++ src/action.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+from functools import wraps
+
 
 class Action:
 	registered = dict()
 
-	def __init__(self, fn, register=True):
+	def __init__(self, fn):
 		self.fn = fn
 
-		if register:
-			Action.registered[fn.__name__] = self
+	def __repr__(self):
+		return '<Action {!r}>'.format(self.fn.__name__)
 
 	def __call__(self, entity):
 		return self.fn(entity)
@@ -22,19 +24,49 @@ class Action:
 			raise TypeError('JSON field action should be a string')
 
 	@classmethod
-	def anonymous(cls, fn):
-		return cls(fn, register=False)
+	def register(cls, fn):
+		"""
+		Instantiate a new Action and register it.
+
+		:param fn: The underlying Python callable object
+		:return: The registered Action instance
+		"""
+
+		if isinstance(fn, Action):
+			action = fn
+			cls.registered[action.fn.__name__] = action
+		else:
+			action = cls(fn)
+			cls.registered[fn.__name__] = action
+
+		return action
+
+	@classmethod
+	def parametrized(cls, fn):
+		"""
+		Instantiates a parametrized action.
+
+		:param fn: The underlying Python callable object
+		:return: Python function returning an Action instance
+		"""
+
+		@wraps(fn)
+		def wrapper(*args, **kwds):
+			action = fn(*args, **kwds)
+			return Action(action)
+
+		return wrapper
 
 
-@Action
+@Action.register
 def nothing(entity):
 	pass
 
 
+@Action.register
+@Action.parametrized
 def debug(message):
-	@Action.anonymous
 	def wrapper(entity):
-		nonlocal message
 		print(message)
 
 	return wrapper
